7336fa6dbdb5cca3e3dc4d5bbd5ed9e08ba41d0a
Gasp763 <gaspardberthe@gmail.com>
2025-12-17 16:48:33 +0100

    Tabs Animation

---
+++ Système de Tabs - Animation/assets/app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const tabLinks = document.querySelectorAll(".tab__el a");
+
+if(tabLinks.length > 0){
+    for (let tabLink of tabLinks) {
+        tabLink.addEventListener("click", tabTrigger);
+    }
+}
+
+function tabTrigger(event){
+    event.preventDefault();
+    
+    const parentTab = event.currentTarget.closest(".tab");
+    
+    const activeLink = parentTab.querySelector(".tab__el--active");
+    activeLink.classList.remove("tab__el--active");
+    
+    event.currentTarget.parentNode.classList.add("tab__el--active");
+
+    const tabs = parentTab.querySelectorAll(".tab__content");
+    const tabActive = parentTab.querySelector(".tab__content--active");
+
+    tabActive.classList.remove("tab__content--active");
+    
+    const tabSelectID = event.currentTarget.getAttribute("href");
+    const tabSelect = parentTab.querySelector(tabSelectID);
+    tabSelect.classList.add("tab__content--active");
+
+    //Ajuster la hauteur du conteneur à la tab active
+
+    if (parentTab.classList.contains("tab--a")){
+        const tabContainerA = document.querySelector(".tab--a .tab__container");
+        tabContainerA.style.height = tabSelect.offsetHeight + "px";
+    }
+
+  
+}
+
+// Définir la taille du conteneur par rapport à la tab active
+const activeTabA = document.querySelector(".tab--a .tab__content--active");
+const tabContainerA = document.querySelector(".tab--a .tab__container");
+tabContainerA.style.height = activeTabA.offsetHeight + "px";
